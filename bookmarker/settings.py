f2e47d0a34bf3d7a2b91f4dcf7408c1d61339ae4
dellsystem <ilostwaldo@gmail.com>
2017-08-18 22:50:32 +0000

    Switch database from sqlite to postgres

--- bookmarker/settings.py
+++ bookmarker/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'bookmarker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bookmarker',
+        'USER': 'bookmarker',
+        'PASSWORD': os.environ.get('BOOKMARKER_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
