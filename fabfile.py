611f815c4e688d9139ed42901661624c86642ec9
dellsystem <ilostwaldo@gmail.com>
2018-01-04 00:20:45 +0000

    Replace backup.sh with fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+import datetime
+
+from fabric.api import *
+
+
+env.use_ssh_config = True
+env.host_string = 'stratus'
+
+
+def up():
+    local('django/manage.py runserver')
+
+
+def get_backup_filename(hostname):
+    return 'backups/{}_{}.json'.format(
+        hostname,
+        datetime.datetime.now().strftime('%Y-%m-%d-%H%M')
+    )
+
+BACKUP_COMMAND = 'python manage.py dumpdata books vocab > '
+def backup():
+    """Does a local database dump. Returns the filename."""
+    local_filename = get_backup_filename(hostname=socket.gethostname())
+    local(BACKUP_COMMAND + local_filename)
+
+    return local_filename
+
+
+def backup_remote():
+    """Does a remote database dump and scps the file. Returns the filename."""
+    remote_filename = get_backup_filename(hostname=env.host_string)
+    print("Remote filename: " + remote_filename)
+
+    with cd('bookmarker'):
+        run(BACKUP_COMMAND + remote_filename)
+        # scp the remote backup file to local.
+        get(remote_filename, remote_filename)
+
+    return remote_filename
