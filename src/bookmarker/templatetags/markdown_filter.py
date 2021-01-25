c7109bee115b7dcbc19ddb3e1a710ae9f8fbeeea
dellsystem <ilostwaldo@gmail.com>
2021-01-24 17:23:43 -0800

    Improve newline handling with markdown
    Closes #136.

--- src/bookmarker/templatetags/markdown_filter.py
+++ src/bookmarker/templatetags/markdown_filter.py
@@ -13,7 +13,7 @@ def markdownify(text):
     return mark_safe(
         markdown.markdown(
             text,
-            extensions=['superscript'],
+            extensions=['superscript', 'nl2br'],
             smart_emphasis=False
         ).replace('    ', '&ensp;')
     )
