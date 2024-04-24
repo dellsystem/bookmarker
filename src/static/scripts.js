d02181450c08d99dc0b6fc0fa8ec661607392ab9
dellsystem <ilostwaldo@gmail.com>
2024-04-24 14:50:12 -0700

    Improve Javascript slug generation
    thx stackoverflow <3
    
    Closes #215.

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,9 +1,54 @@
+function slugify(text) {
+    /* this comes from stack overflow lol
+    https://stackoverflow.com/questions/54743952/javascript-slug-working-for-non-latin-characters-also
+    */
+    text = text.toLowerCase().trim();
+    const sets = [
+        {to: 'a', from: '[ÀÁÂÃÄÅÆĀĂĄẠẢẤẦẨẪẬẮẰẲẴẶἀ]'},
+        {to: 'c', from: '[ÇĆĈČ]'},
+        {to: 'd', from: '[ÐĎĐÞ]'},
+        {to: 'e', from: '[ÈÉÊËĒĔĖĘĚẸẺẼẾỀỂỄỆ]'},
+        {to: 'g', from: '[ĜĞĢǴ]'},
+        {to: 'h', from: '[ĤḦ]'},
+        {to: 'i', from: '[ÌÍÎÏĨĪĮİỈỊ]'},
+        {to: 'j', from: '[Ĵ]'},
+        {to: 'ij', from: '[Ĳ]'},
+        {to: 'k', from: '[Ķ]'},
+        {to: 'l', from: '[ĹĻĽŁ]'},
+        {to: 'm', from: '[Ḿ]'},
+        {to: 'n', from: '[ÑŃŅŇ]'},
+        {to: 'o', from: '[ÒÓÔÕÖØŌŎŐỌỎỐỒỔỖỘỚỜỞỠỢǪǬƠ]'},
+        {to: 'oe', from: '[Œ]'},
+        {to: 'p', from: '[ṕ]'},
+        {to: 'r', from: '[ŔŖŘ]'},
+        {to: 's', from: '[ßŚŜŞŠȘ]'},
+        {to: 't', from: '[ŢŤ]'},
+        {to: 'u', from: '[ÙÚÛÜŨŪŬŮŰŲỤỦỨỪỬỮỰƯ]'},
+        {to: 'w', from: '[ẂŴẀẄ]'},
+        {to: 'x', from: '[ẍ]'},
+        {to: 'y', from: '[ÝŶŸỲỴỶỸ]'},
+        {to: 'z', from: '[ŹŻŽ]'},
+        {to: '-', from: '[·/_,:;\']'}
+  ];
+
+  sets.forEach(set => {
+    text = text.replace(new RegExp(set.from,'gi'), set.to)
+  });
+
+  return text
+    .replace(/\s+/g, '-')    // Replace spaces with -
+    .replace(/[^-a-zа-я\u0370-\u03ff\u1f00-\u1fff]+/g, '') // Remove all non-word chars
+    .replace(/--+/g, '-')    // Replace multiple - with single -
+    .replace(/^-+/, '')      // Trim - from start of text
+    .replace(/-+$/, '')      // Trim - from end of text
+}
+
+
 function fillSlugFromInput(inputName, inputPrefix) {
     var value = document.getElementById('id_' + inputPrefix + inputName).value;
-    // Replace spaces with - and remove non-word chars
-    var slug = value.toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '');
-    document.getElementById('id_' + inputPrefix + 'slug').value = slug;
+    document.getElementById('id_' + inputPrefix + 'slug').value = slugify(value);
 }
+
 
 function fixOcr(elementId) {
     var field = document.getElementById(elementId);
