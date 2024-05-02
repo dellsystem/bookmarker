42e91972de6fd03b38c47d45840f01c2eba53783
dellsystem <ilostwaldo@gmail.com>
2024-05-02 12:16:57 -0700

    Fix bug with post-delete signal handler for books
    This is a little tricky to explain but basically I added in a signal
    handler a while back to make sure that the related bookdetails is
    deleted when the book is deleted (since models.CASCADE doesn't do that
    by default for OneToOneFields). But when deleting from the edit book
    page, we are deleting the BookDetails and not the Book (I think it's
    more logical this way since then it tells us all the objects that will
    be deleted in the admin interface). So there was an error being thrown
    about the BookDetails already being deleted by the time the signal
    handler is run. So this seems to fix it.

--- src/books/signal_handlers.py
+++ src/books/signal_handlers.py
@@ -6,6 +6,8 @@ from books.models import Book, BookDetails
 
 @receiver(post_delete, sender=Book)
 def delete_related_book_details(sender, instance, **kwargs):
-    if instance.details is not None:
-        details = BookDetails.objects.get(pk=instance.details.pk)
-        details.delete()
+    if instance.details_id is not None:
+        # It won't exist if we're deleting the BookDetails directly
+        details = BookDetails.objects.filter(pk=instance.details_id).first()
+        if details:
+            details.delete()
